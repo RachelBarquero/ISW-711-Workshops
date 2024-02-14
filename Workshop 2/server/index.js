9c1cb37de20c166d3ebd78839405e4143ca8f85e
Rachel Barquero Varela <82628293+RachelBarquero@users.noreply.github.com>
2024-02-14 01:29:18 -0600

    Add files via upload

---
+++ Workshop 2/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors({
+  domains: '*',
+  methods: "*"
+}));
+
+app.get('/paises', async (req, res) => {
+  try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      res.json(data);
+  } catch (error) {
+      console.error('Error al obtener los países:', error);
+      res.status(500).json({ error: 'Error al obtener los países' });
+  }
+});
+
+app.listen(3001, () => console.log(`3001 exitoso!`))
